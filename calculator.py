2e409bf4c6624d6ffa5132a2c1f1eaa9ffe37ce8
salma578 <salmashaik12630@gmail.com>
2026-04-09 18:55:58 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,48 @@
+# Calculator CLI App
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+while True:
+    print("\nCalculator Menu")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '5':
+        print("Exiting Calculator...")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+
+    else:
+        print("Invalid choice")
